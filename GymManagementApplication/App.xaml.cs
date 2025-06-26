51b002a2c4abb2cf4747973bbb1469395f599742
Acer <Acer@DESKTOP-I58A6R5>
2025-06-26 17:12:57 +0700

    demo mainwindow

--- GymManagementApplication/App.xaml.cs
+++ GymManagementApplication/App.xaml.cs
@@ -9,6 +9,23 @@ namespace GymManagementApplication
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            LoginWindow loginWindow = new LoginWindow();
+            bool? result = loginWindow.ShowDialog();
+
+            if(result ==true)//login successful
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+            }
+            else //login failed or cancelled
+            {
+                Application.Current.Shutdown(); //close the application
+            }
+        }
     }
 
 }
